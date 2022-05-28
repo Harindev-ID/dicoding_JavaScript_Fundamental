81b6872204e3a7589a50d3ce5612725435a4ab27
adityadenny <adityadenny6@gmail.com>
2022-05-28 21:41:49 +0700

    learn if else statement

---
+++ if-else_statement.js
@@ -0,0 +1,8 @@
+const isRaining = true;
+
+console.log("Preparation before going to work");
+
+if (isRaining) {
+    console.log("Today is rain, please take an umbrella");
+}
+console.log("Going to work");
