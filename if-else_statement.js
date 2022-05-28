d833a3a1b2481978e42f20c965a604bf9f9bc41e
adityadenny <adityadenny6@gmail.com>
2022-05-28 21:53:23 +0700

    if else statement add cases

--- if-else_statement.js
+++ if-else_statement.js
@@ -1,8 +1,33 @@
-const isRaining = true;
+const isRaining = false;
 
 console.log("Preparation before going to work");
 
 if (isRaining) {
     console.log("Today is rain, please take an umbrella");
+} else {
+    console.log("Just go, the weather is sunny today ")
 }
+
+
 console.log("Going to work");
+
+
+let language = "Japanese";
+let greeting = "Selamat Pagi"
+
+if(language === "English") {
+    greeting = "Good Morning!";
+} else if(language === "French") {
+    greeting = "Bonjour!"
+} else if(language === "Japanese") {
+    greeting = "Ohayou Gozaimasu!"
+}
+
+console.log(greeting);
+
+// condition ? true expression : false expression
+
+const isMember = false;
+const discount = isMember ? 0.9 : 1;
+console.log(`Anda mendapatkan diskon sebesar ${discount * 100}%`)
+
