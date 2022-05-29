72b00d3ff5cf2725ef6557ecf0258f4da13ef041
adityadenny <adityadenny6@gmail.com>
2022-05-29 08:10:13 +0700

    learn loop, foor loop & for of

---
+++ loop.js
@@ -0,0 +1,15 @@
+// for loop
+
+for(let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+for(let i = 10; i > 0  ; i--) {
+    console.log(i);
+}
+
+let myArray = ["Luke", "Han", "Chewbacca", "Leia"];
+
+for(const arrayItem of myArray) {
+    console.log(arrayItem)
+}
