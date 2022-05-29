2bd74b88f3d7eb95311f4895201a8b61c6cdc66c
adityadenny <adityadenny6@gmail.com>
2022-05-29 15:38:18 +0700

    submit quis scoreChecker if/else statement

---
+++ kuis1.js
@@ -0,0 +1,28 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+
+let firstName = "Denny";
+let lastName = "Aditya";
+let age = 30;
+let isMarried = true
+
+console.log(firstName + lastName + age + isMarried);
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+ module.exports = {
+    firstName, lastName, age, isMarried,
+  };
+  
+
+
