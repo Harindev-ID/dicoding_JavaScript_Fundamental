2e6afaf6c40774aa49ad86bf41677c056d41c4f0
adityadenny <adityadenny6@gmail.com>
2022-05-30 22:38:29 +0700

    commit

---
+++ test1.js
@@ -0,0 +1,18 @@
+const stock = 0;
+const milkNeeded = 25;
+
+if (stock > milkNeeded) {
+  stock = stock - milkNeeded;
+  console.log('Processing your order...');
+} else {
+  console.log('Out of Stock!');
+}
+
+console.log('Thank you');
+
+const firstString = "Hello";
+const secondString = "JavaScript";
+
+console.log(firstString + secondString);
+
+// nisy vp,,oy
