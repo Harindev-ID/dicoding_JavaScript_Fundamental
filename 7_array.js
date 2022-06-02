509daee3e477e4ec6fa116b61e1cdbf471d9fcea
adityadenny <adityadenny6@gmail.com>
2022-06-02 08:54:00 +0700

    akses nilai array lebih dari indexnya

--- 7_array.js
+++ 7_array.js
@@ -13,3 +13,24 @@ console.log(myArray[1]);
 /* output:
 42.5
 */
+
+// Mengakses nilai array lebih dari indexnya
+
+let myArray2 = ["Cokelat", 42.5, 22, true, "Programming"];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+console.log(myArray[4]);
+console.log(myArray[5]);
+console.log("Panjang nilai myArray adalah " + myArray.length + ".");
+
+/* output:
+Cokelat
+42.5
+22
+true
+Programming
+undefined
+Panjang nilai myArray adalah 5.
+*/
