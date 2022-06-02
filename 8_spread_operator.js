21aec8f0a1835672ff97fd36552649e081aa2c0e
adityadenny <adityadenny6@gmail.com>
2022-06-02 09:22:13 +0700

    spread operator

---
+++ 8_spread_operator.js
@@ -0,0 +1,49 @@
+/*
+Masih terkait dengan array, ES6 memiliki fitur menarik untuk membantu pengelolaan array menjadi lebih mudah, yaitu spread operator.
+
+Sesuai namanya “spread”, fitur ini digunakan untuk menyebarkan nilai array atau lebih tepatnya iterable object menjadi beberapa elemen. 
+Spread operator dituliskan dengan tiga titik (...). Mari kita lihat contoh kode berikut:
+*/
+
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+
+console.log(...favorites);
+
+/* output
+Seafood Salad Nugget Soup
+*/
+
+/* Spread operator dapat digunakan untuk menggabungkan dua buah array ke dalam array baru. 
+Jika tidak menggunakan spread operator ini maka hasilnya akan seperti ini: 
+*/
+
+const favorites1 = ["Seafood", "Salad", "Nugget", "Soup"];
+const others = ["Cake", "Pie", "Donut"];
+
+const allFavorites = [favorites1, others];
+
+console.log(allFavorites);
+
+/* output
+[
+  [ 'Seafood', 'Salad', 'Nugget', 'Soup' ],
+  [ 'Cake', 'Pie', 'Donut' ]
+]
+*/
+
+/*
+Nilai array tidak akan tergabung. Alih-alih menggabungkan nilainya, 
+variabel allFavorites menjadi array baru yang menampung dua array di dalamnya. 
+Nah, lantas bagaimana jika kita mencoba menggunakan spread operator?
+*/
+
+const favorites2 = ["Seafood", "Salad", "Nugget", "Soup"];
+const others1 = ["Cake", "Pie", "Donut"];
+
+const allFavorites1 = [...favorites2, ...others1];
+
+console.log(allFavorites1);
+
+/* output
+[ 'Seafood', 'Salad', 'Nugget', 'Soup', 'Cake', 'Pie', 'Donut' ]
+*/
