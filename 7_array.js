929ff1151214d49626256903132def14673034fa
adityadenny <adityadenny6@gmail.com>
2022-06-02 08:52:39 +0700

    mengakses array

--- 7_array.js
+++ 7_array.js
@@ -4,3 +4,12 @@ console.log(myArray);
 /* output:
 [ 'Cokelat', 42.5, 22, true, 'Programming' ]
 */
+
+// mengakses array
+
+let myArray1 = ["Cokelat", 42.5, 22, true, "Programming"];
+console.log(myArray[1]);
+
+/* output:
+42.5
+*/
