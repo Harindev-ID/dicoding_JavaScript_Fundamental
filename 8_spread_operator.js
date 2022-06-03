83cf68afee07a7551743f308fe8af67dcdf250e7
adityadenny <adityadenny6@gmail.com>
2022-06-03 09:25:09 +0700

    destructuring object & array

--- 8_spread_operator.js
+++ 8_spread_operator.js
@@ -47,3 +47,20 @@ console.log(allFavorites1);
 /* output
 [ 'Seafood', 'Salad', 'Nugget', 'Soup', 'Cake', 'Pie', 'Donut' ]
 */
+
+
+/**
+ Selain array, spread operator juga bisa digunakan untuk object literals. 
+ Hal ini memungkinkan kita dapat menggabungkan beberapa object dengan kode yang lebih ringkas.
+ */
+
+ const obj1 = { firstName: 'Obi-Wan', age: 30 };
+const obj2 = { lastName: 'Kenobi', gender: 'M' };
+
+const newObj = { ...obj1, ...obj2 };
+
+console.log(newObj);
+
+/* output
+{ firstName: 'Obi-Wan', age: 30, lastName: 'Kenobi', gender: 'M' }
+*/
