be232bf2b2caa84460f52754dea5badd5d4cd125
adityadenny <adityadenny6@gmail.com>
2022-06-06 08:18:11 +0700

    destructuring assignment before es6

--- 11_destructuring_array.js
+++ 11_destructuring_array.js
@@ -49,3 +49,33 @@ console.log(favorites2);
 Seafood
 Salad
 */
+
+/**
+ * Array destructuring assignment sangat berguna ketika kita hendak menukar nilai antara dua variabel. 
+ * Sebelum ES6, untuk melakukan hal ini kita menggunakan cara manual menggunakan algoritma sorting seperti ini:
+ */
+
+ var a = 1;
+ var b = 2;
+ var temp;
+  
+ console.log("Sebelum swap");
+ console.log("Nilai a: " + a);
+ console.log("Nilai b: " + b);
+  
+ temp = a;
+ a = b;
+ b = temp;
+  
+ console.log("Setelah swap");
+ console.log("Nilai a: " + a);
+ console.log("Nilai b: " + b);
+  
+ /* output
+ Sebelum swap
+ Nilai a: 1
+ Nilai b: 2
+ Setelah swap
+ Nilai a: 2
+ Nilai b: 1
+ */
