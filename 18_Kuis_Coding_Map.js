02615ed3bffb8a73a66a1d826bb029bc0afb0c72
adityadenny <adityadenny6@gmail.com>
2022-06-14 03:22:14 +0700

    Kuis coding map

---
+++ 18_Kuis_Coding_Map.js
@@ -0,0 +1,21 @@
+/**
+ * TODO:
+ * 1. Buatlah variabel currency yang merupakan Map dengan kriteria:
+ *   - key "USD", value 14000
+ *   - key "JPY", value 131
+ *   - key "SGD", value 11000
+ *   - key "MYR", value 3500
+ * 2. Buatlah variabel priceInIDR yang bernilai dari hasil perkalian:
+ *     - priceInJPY dengan nilai currency JPY
+ */
+
+ const priceInJPY = 5000;
+
+ // TODO
+ 
+ 
+ /**
+  * Jangan hapus kode di bawah ini
+  */
+ module.exports = { currency, priceInJPY, priceInIDR };
+ 
