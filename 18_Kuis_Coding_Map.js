35b360cd68df860a56e0174bb2bdcef22467d7e4
adityadenny <adityadenny6@gmail.com>
2022-06-14 04:33:25 +0700

    Kuis coding map

--- 18_Kuis_Coding_Map.js
+++ 18_Kuis_Coding_Map.js
@@ -12,6 +12,15 @@
  const priceInJPY = 5000;
 
  // TODO
+
+ const currency = new Map([
+    ['USD', 14000],
+    ['JPY', 131],
+    ['SGD', 11000],
+    ['MYR', 3500],
+]);
+
+console.log(myMap);
  
  
  /**
