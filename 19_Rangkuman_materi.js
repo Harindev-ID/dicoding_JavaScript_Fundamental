207c89683e07902f2d64f5dffbb928b0ca0c3e5c
adityadenny <adityadenny6@gmail.com>
2022-06-15 07:32:49 +0700

    rangkuman materi

--- 19_Rangkuman_materi.js
+++ 19_Rangkuman_materi.js
@@ -5,12 +5,18 @@ JavaScript memiliki tipe data lain yang dapat menyimpan banyak data sekaligus, c
 
 Beberapa hal yang telah kita bahas pada modul ini, antara lain:
 
-Object adalah tipe data kompleks untuk menyimpan banyak data dengan format key-value. Key pada object harus berupa string.
-Array digunakan untuk menyimpan banyak data. Data pada array disusun secara berurutan dan diakses menggunakan index.
-Map menyimpan banyak data dengan format key-value seperti object. Namun, key pada Map dapat bertipe apa pun.
-Set digunakan untuk menyimpan banyak data secara unik, tidak ada duplikasi, tidak berurutan, dan tidak diindeks.
+- Object adalah tipe data kompleks untuk menyimpan banyak data dengan format key-value. Key pada object harus berupa string.
+- Array digunakan untuk menyimpan banyak data. Data pada array disusun secara berurutan dan diakses menggunakan index.
+- Map menyimpan banyak data dengan format key-value seperti object. Namun, key pada Map dapat bertipe apa pun.
+- Set digunakan untuk menyimpan banyak data secara unik, tidak ada duplikasi, tidak berurutan, dan tidak diindeks.
 
 
 Materi Pendukung
 Berikut ini adalah beberapa sumber materi tambahan yang bisa Anda pelajari:
+- Object : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+- Array : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+- Map : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+- Set : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+- Weakmap and Weakset : https://javascript.info/weakmap-weakset
+- Garbage Collection : https://javascript.info/garbage-collection
  */
