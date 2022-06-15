374aad7ccc1547ad67dbdfde94d6d931a58c33d0
adityadenny <adityadenny6@gmail.com>
2022-06-15 07:49:35 +0700

    declaring function & expression Function

--- 21_Declaring_function.js
+++ 21_Declaring_function.js
@@ -95,3 +95,75 @@ greeting("Harry", "French");
 /* output
 Bonjour Harry!
 */
+
+
+/**
+ * Satu hal lagi, function dapat menghasilkan output atau mengembalikan sebuah nilai. Dengan nilai kembalian, 
+ * kita dapat membuat function yang berfungsi untuk melakukan perhitungan matematika dan 
+ * hasilnya dapat kita masukkan ke dalam sebuah variabel. Contohnya seperti ini:
+ */
+
+ function multiply(a, b) {
+    return a * b;
+}
+
+let result = multiply(10, 2)
+console.log(result)
+
+/* output
+20
+*/
+
+/**
+ * Agar fungsi bisa mengembalikan nilai, gunakan keyword return diikuti dengan nilai yang akan dikembalikan. Nilai kembalian tidak hanya number, 
+ * bisa juga berupa string, boolean, object, array, atau tipe yang lain. 
+ * 
+ * Seperti inilah fungsi greeting() jika kita ubah agar mengembalikan nilai string:
+ */
+
+ function greeting3(name, language) {
+    if(language === "English") {
+        return `Good Morning ${name}!`
+    } else if (language === "French") {
+        return `Bonjour ${name}!`;
+    } else {
+        return `Selamat Pagi ${name}!`;
+    }
+}
+
+let greetingMessage = greeting3("Harry", "French");
+console.log(greetingMessage);
+
+/* output
+Bonjour Harry!
+
+- Yang perlu kita perhatikan lagi, ketika statement return tereksekusi, maka fungsi akan langsung terhenti dan mengembalikan nilai.
+*/
+
+
+/**
+ * Expression Function
+Cara lain untuk membuat sebuah fungsi pada JavaScript adalah expression function. 
+Ingat kembali bahwa expression adalah kode atau instruksi yang mengembalikan nilai, 
+sehingga expression function bisa disimpan dalam sebuah variabel.
+
+Pada expression function umumnya kita tidak perlu menuliskan nama fungsinya. 
+Fungsi yang tidak bernama juga dikenal dengan anonymous function. 
+
+Berikut ini merupakan contoh penulisan expression function:
+ */
+const greeting4 = function(name, language) {
+    if(language === "English") {
+        return "Good Morning " + name + "!";
+    } else if (language === "French") {
+        return "Bonjour " + name + "!";
+    } else {
+        return "Selamat Pagi " + name + "!";
+    }
+}
+
+console.log(greeting4('Ron', 'English'));
+
+/* output
+Good Morning Ron!
+ */
