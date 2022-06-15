6f33efa4db8243b7e2483a68b59f0dd813aa7273
adityadenny <adityadenny6@gmail.com>
2022-06-15 09:03:04 +0700

    Arrow Function

--- 23_Arrow_Function.js
+++ 23_Arrow_Function.js
@@ -78,4 +78,13 @@ sayHello();
 /* output
 Nama saya Leia
 Selamat pagi semuanya!
+
+Ketika sebuah fungsi perlu mengembalikan nilai, kita tidak perlu lagi menuliskan return (hanya bekerja untuk fungsi satu baris).
+ */
+
+const multiply = (a, b) => a * b;
+console.log(multiply(3, 4));
+
+/* output
+12
  */
