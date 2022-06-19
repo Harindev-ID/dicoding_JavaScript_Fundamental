25763e296c3e7f23197970a8b74dc90bfed69aa7
adityadenny <adityadenny6@gmail.com>
2022-06-19 07:29:20 +0700

    property dan method

--- 31_property_&_method.js
+++ 31_property_&_method.js
@@ -9,6 +9,7 @@ Misalkan isi pesan, list penerima, data pengirim, perintah kirim, dll.
 Sebagai contoh:
  */
 
+/*
 // cara 1
 class Mail {
     constructor() {
@@ -23,13 +24,44 @@ class Mail {
 }
  
 // cara 2
-function Mail() {
+function mail1() {
     this.from = 'pengirim@dicoding.com';
     this.contacts = [];
     this.yourOtherPrototype = 'the values';
 };
  
-Mail.prototype.sendMessage = function (msg, to) {
+mail1.prototype.sendMessage = function (msg, to) {
     console.log(`you send: ${msg} to  ${to} from ${this.from}`);
     this.contacts.push(to); // menyimpan kontak baru
 };
+
+*/
+
+/**
+ * Contoh class di atas memiliki 3 (tiga) property dan method. 
+ * Yang perlu diingat, this merupakan representasi bahwasanya variable yang ditunjuk adalah atribute yang bersifat global, 
+ * dan menempel dengan objek tersebut. 
+ * 
+ * Sehingga, variabel dapat diakses dari method ataupun property di dalam kelas tersebut dengan menambahkan this di depannya. 
+ * Sebagai gambaran, kita akan coba ubah kode di atas menjadi seperti berikut ini.
+ */
+
+ class Mail {
+    constructor() {
+        this.from = 'pengirim@dicoding.com';
+        this.contacts = [];
+    }
+    sendMessage(msg, to, from) {
+        console.log(`you send: ${msg} to ${to} from ${from}`);
+        // from di sini merujuk ke `from` parameter, bukan ke `from` dari global value yaitu pengirim@dicoding.com
+        this.contacts.push(to);
+    };
+}
+
+const mail1 = new Mail();
+mail1.sendMessage('hallo', 'penerima@dicoding.com', 'aku@gmail.com');
+
+/**
+you send: hallo to penerima@dicoding.com from aku@gmail.com
+**/
+
