ab763aafe64eb3d09d63a46f9edc4ddf55d86bdb
adityadenny <adityadenny6@gmail.com>
2022-06-22 07:51:20 +0700

    4 Pilar OOP

--- 4_pilar_OOP.js
+++ 4_pilar_OOP.js
@@ -9,7 +9,7 @@ Artinya objek lain tidak bisa mengakses atau mengubah nilai dari property secara
 Pada contoh kasus Mail kita tidak bisa langsung mengubah daftar contact, 
 namun kita bisa menambahkannya melalui fungsi saat kirim pesan atau mengambil data tersebut melalui method showAllContacts.
 
-https://www.dicoding.com/academies/256/tutorials/13937?from=15895#
+https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:8c95641c84aae61c3219314b96c9b51f20220613105529.png
 
 class Mail{
     constructor(author) {
@@ -36,3 +36,18 @@ namun kita tidak perlu tahu seperti apa proses enkripsi dan dekripsi isi pesan,
 atau bagaimana sebuah daftar contact dapat bertambah.
  */
 
+/**
+ * Inheritance
+ * 
+Beberapa objek bisa memiliki beberapa karakteristik atau perilaku yang sama, 
+tetapi mereka bukanlah objek yang sama. 
+
+Di sinilah inheritance atau pewarisan berperan. 
+SMS dan jenis pesan lainnya memiliki karakteristik umum yang dimiliki juga oleh jenis pesan lainnya, 
+seperti memiliki konten pesan, alamat/nomor pengirim, alamat/nomor penerima, dsb. 
+Maka dari itu, Email sebagai objek turunan (subclass) mewarisi semua sifat dan perilaku dari objek induknya (superclass) Mail. 
+Begitu juga dengan objek Whatsapp juga mewarisi sifat dan perilaku yang sama. 
+Namun, whatsapp bisa membuat grup, mengirim broadcast message sedangkan Email tidak.
+
+https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:1eef923564c565cea0e9992208e78fab20220613105547.png
+*/
