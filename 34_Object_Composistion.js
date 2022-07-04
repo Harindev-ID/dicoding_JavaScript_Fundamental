f949462a039a858b0967b830b5bb7614223cde47
adityadenny <adityadenny6@gmail.com>
2022-07-05 06:52:17 +0700

    .

--- 34_Object_Composistion.js
+++ 34_Object_Composistion.js
@@ -59,4 +59,5 @@ const YourClassName = (paramA, paramB) => {
 
 /*
 Sebagai contoh, dari hirarki Mail yang sudah kita buat sebelumnya. kita akan merombak dan membuatnya dengan pendekatan Object composition.
- */
+ 
+.*/
