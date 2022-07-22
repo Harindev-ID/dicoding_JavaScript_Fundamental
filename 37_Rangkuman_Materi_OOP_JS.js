52bdf05c727b528939caddae91fe9986fb8c0146
adityadenny <adityadenny6@gmail.com>
2022-07-22 10:01:25 +0700

    -

--- 37_Rangkuman_Materi_OOP_JS.js
+++ 37_Rangkuman_Materi_OOP_JS.js
@@ -13,4 +13,4 @@ Berikut ini beberapa materi tambahan yang bisa Anda pelajari untuk mendalami sep
 
 Object-oriented JavaScript for beginners = https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
 A Guide to Object-Oriented Programming in JavaScript = https://betterprogramming.pub/object-oriented-programming-in-javascript-b3bda28d3e81
- */..
+ */
