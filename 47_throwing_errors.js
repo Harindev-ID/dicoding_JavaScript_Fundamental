bbf6a8b8c9ff6563e02a9315738971e08191196c
adityadenny <adityadenny6@gmail.com>
2022-08-16 07:47:27 +0700

    throwing erros syntax error

--- 47_throwing_errors.js
+++ 47_throwing_errors.js
@@ -48,14 +48,41 @@ Unexpected token b in JSON at position 2
 Lalu, bagaimana jika json yang didapat adalah seperti ini?
  */
 
+// let json = '{ "age": 20 }';
+ 
+// try {
+//     let user = JSON.parse(json);
+ 
+//     console.log(user.name); // undefined
+//     console.log(user.age);  // 20
+// } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+// }
+
+/**
+ Secara sintaksis, kode di atas tidak menimbulkan eror, sehingga blok catch akan diabaikan. 
+ Namun, tidak adanya properti name pada json sebenarnya sama saja dengan eror karena akan berdampak pada jalannya program kita.
+
+Untuk mengatasinya, kita bisa menggunakan throw. Operator ini akan “melemparkan” eror pada program, 
+sehingga eksekusi kode akan masuk pada blok catch. Berikut ini adalah contoh mengimplementasikan throw untuk menimbulkan eror kita sendiri:
+ */
+
 let json = '{ "age": 20 }';
  
 try {
     let user = JSON.parse(json);
  
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+ 
     console.log(user.name); // undefined
     console.log(user.age);  // 20
 } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+    console.log(`JSON Error: ${error.message}`);
 }
+ 
+/* output
+JSON Error: 'name' is required.
+*/
