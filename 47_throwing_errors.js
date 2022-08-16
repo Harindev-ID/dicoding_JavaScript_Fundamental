37500b48c31cc122b04841ed15b2207c86370871
adityadenny <adityadenny6@gmail.com>
2022-08-16 07:44:40 +0700

    throwing erros syntax error

--- 47_throwing_errors.js
+++ 47_throwing_errors.js
@@ -1,6 +1,20 @@
 // Sekarang kita lihat implementasi try-catch pada kasus yang lebih umum. Perhatikan kode berikut:
 
-let json = '{ "name": "Yoda", "age": 20 }';
+// let json = '{ "name": "Yoda", "age": 20 }';
+ 
+// try {
+//     let user = JSON.parse(json);
+ 
+//     console.log(user.name);
+//     console.log(user.age);
+// } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+// }
+
+//---------------------------------------------------
+
+let json = '{ bad json }';
  
 try {
     let user = JSON.parse(json);
@@ -11,3 +25,9 @@ try {
     console.log(error.name);
     console.log(error.message);
 }
+ 
+ 
+/* output
+SyntaxError
+Unexpected token b in JSON at position 2
+*/
