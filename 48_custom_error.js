9bd553ac3792acb9246054735b219866935b837c
adityadenny <adityadenny6@gmail.com>
2022-08-18 09:00:54 +0700

    custom error

---
+++ 48_custom_error.js
@@ -0,0 +1,7 @@
+/**
+Custom Error
+Setelah menangani eror, pada materi ini kita akan mempelajari bagaimana membuat eror sendiri. 
+Ketika mengembangkan suatu aplikasi, akan ada banyak sekali kemungkinan munculnya eror. Seringkali, kita membutuhkan kelas eror sendiri untuk menunjukkan kesalahan yang spesifik dan tidak tersedia dalam kelas Error bawaan dari JavaScript.
+
+Mari lihat lagi kode kita sebelumnya.
+ */
