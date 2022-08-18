f097094179d8b80089a2e8591d731339aa965215
adityadenny <adityadenny6@gmail.com>
2022-08-18 09:01:19 +0700

    custom error

--- 48_custom_error.js
+++ 48_custom_error.js
@@ -5,3 +5,24 @@ Ketika mengembangkan suatu aplikasi, akan ada banyak sekali kemungkinan munculny
 
 Mari lihat lagi kode kita sebelumnya.
  */
+
+let json = '{ "age": 30 }';
+ 
+try {
+    let user = JSON.parse(json);
+ 
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+ 
+    console.log(user.name);
+    console.log(user.age);
+} catch (error) {
+    if (error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if (error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
+}
