8ac1a228374dc7f4453fd8221fbfea409775de07
adityadenny <adityadenny6@gmail.com>
2022-08-23 21:20:11 +0700

    kuis

--- 49_kuis_custom_error.js
+++ 49_kuis_custom_error.js
@@ -44,9 +44,23 @@
 
 
 // TODO 1
-
+class ValidationError extends Error {
+    constructor(message) {
+        super(message)
+        this.name = 'ValidationError'
 // TODO 2
+const validateNumberInput = (a, b, c) => {
 
+    if (typeof a !== "number") {
+        throw new ValidationError('Argumen pertama harus number')
+    }
+    if (typeof b !== "number") {
+      throw new ValidationError('Argumen kedua harus number')
+    }
+    if (typeof c !== "number ") {
+      throw new ValidationError ('Argumen ketiga harus number')
+    }
+}
 
 const detectTriangle = (a, b, c) => {
     // TODO 3
@@ -65,5 +79,4 @@ const detectTriangle = (a, b, c) => {
   /**
    * Jangan hapus kode di bawah ini
    */
-  module.exports = { ValidationError, validateNumberInput, detectTriangle };
-  
+   module.exports = { ValidationError, validateNumberInput, detectTriangle };
