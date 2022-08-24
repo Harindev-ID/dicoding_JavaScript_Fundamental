b01b4b4714c0498301d615fb996940719f43bee1
adityadenny <adityadenny6@gmail.com>
2022-08-24 07:51:33 +0700

    callback function

--- 51_callback_function.js
+++ 51_callback_function.js
@@ -88,5 +88,29 @@ Menjadi:
 orderCoffee(coffee => {
     console.log(coffee);
 });
+
+Sehingga ketika dijalankan akan sesuai dengan harapan kita.
 */
+
+const orderCoffee = callback => {
+    let coffee = null;
+    console.log("Sedang membuat kopi, silakan tunggu...");
+    setTimeout(() => {
+        coffee = "Kopi sudah jadi!";
+        callback(coffee);
+    }, 3000);
+}
+ 
+ 
+orderCoffee(coffee => {
+    console.log(coffee);
+});
+ 
+ 
+/* output
+Sedang membuat kopi, silakan tunggu...
+---- setelah 3 detik ----
+Kopi sudah jadi!
+*/
+
 
