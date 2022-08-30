9b63a528442a826d7f151dc30560e143e470b01d
adityadenny <adityadenny6@gmail.com>
2022-08-30 07:29:14 +0700

    Rangkuman Materi NPM

--- 63_uninstall_package.js
+++ 63_uninstall_package.js
@@ -32,3 +32,14 @@ Perintah uninstall juga memiliki beberapa alias, sehingga dapat lebih cepat dala
 
 npm uninstall -h
 */
+
+// ---------------------------------------------------
+
+/**
+Rangkuman Materi
+Pada modul ini kita telah membahas seputar Node Package Manager (NPM) untuk mengelola kode atau package milik orang lain ke dalam project kita. Seharusnya Anda sudah bisa menggunakan perintah dasar NPM seperti:
+
+Menginstal package melalui NPM.
+Menggunakan package untuk membantu pengembangan aplikasi.
+Menghapus proyek yang sudah tidak digunakan.
+ */
