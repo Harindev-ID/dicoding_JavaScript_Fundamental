5983bf0a4c415d8df1506632de377d3d68b43637
adityadenny <adityadenny6@gmail.com>
2022-09-08 00:07:11 +0700

    ok

--- etc.js
+++ etc.js
@@ -1,15 +1,3 @@
 function multiply(num) {
     total = num * num;
 }
-
-<<<<<<< HEAD
-const result = multiply(3);
-
-console.log(result);
-//......
-=======
-console.log('Thank you');
-
-//testing ...
->>>>>>> f470382deef1b9123043c2520351e9897045b709
-// testing
